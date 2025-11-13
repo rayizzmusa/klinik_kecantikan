fbcf5fd15d1ffb4972dd8075d23eaa11340ee13b
Mutiara <mutiara.joesoef@gmail.com>
2025-11-13 21:53:28 +0700

    update

--- klinik1.sql
+++ klinik1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 13, 2025 at 02:52 PM
+-- Generation Time: Nov 13, 2025 at 05:08 AM
 -- Server version: 8.0.30
--- PHP Version: 8.1.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,30 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `master_treatment`
---
-
-CREATE TABLE `master_treatment` (
-  `id` int NOT NULL,
-  `layanan` varchar(255) NOT NULL,
-  `hapus` int NOT NULL,
-  `harga` varchar(255) NOT NULL,
-  `deskripsi` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `master_treatment`
---
-
-INSERT INTO `master_treatment` (`id`, `layanan`, `hapus`, `harga`, `deskripsi`) VALUES
-(1, 'Hair Treatment', 0, '100.000', 'Perawatan rambut'),
-(2, 'Salmon DNA', 0, '1.000.000', 'Kulit cerah bagaikan ikan salmon'),
-(5, 'Facial', 1, '150.000', 'Biar G jerawatan'),
-(6, 'Facial Treatment', 0, '150.000', 'Rawat wajahmu biar ga jerawatan');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `transaction`
 --
 
@@ -61,6 +37,16 @@ CREATE TABLE `transaction` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`id`, `id_user`, `username`, `id_treatment`, `layanan`, `hapus`, `created_at`) VALUES
+(1, 1, 'luluk', 6, 'Hair Treatment', 0, '2025-11-13 03:40:33'),
+(2, 1, 'luluk', 11, 'Facial', 0, '2025-11-13 04:31:43'),
+(3, 3, 'mayden', 2, 'Treatment Acne', 0, '2025-11-13 04:37:21'),
+(4, 4, 'syifa', 9, 'Facial', 0, '2025-11-13 05:04:39');
+
 -- --------------------------------------------------------
 
 --
@@ -69,7 +55,7 @@ CREATE TABLE `transaction` (
 
 CREATE TABLE `treatment` (
   `id` int NOT NULL,
-  `id_layanan` int NOT NULL,
+  `layanan` varchar(255) NOT NULL,
   `harga` varchar(255) NOT NULL,
   `jam` time NOT NULL,
   `hapus` int NOT NULL
@@ -79,8 +65,18 @@ CREATE TABLE `treatment` (
 -- Dumping data for table `treatment`
 --
 
-INSERT INTO `treatment` (`id`, `id_layanan`, `harga`, `jam`, `hapus`) VALUES
-(1, 1, '100.000', '21:00:00', 0);
+INSERT INTO `treatment` (`id`, `layanan`, `harga`, `jam`, `hapus`) VALUES
+(1, 'Treatment Acne', '250.000', '08:00:00', 0),
+(2, 'Treatment Acne', '250.000', '10:00:00', 0),
+(3, 'Treatment Acne', '250.000', '12:00:00', 0),
+(4, 'Treatment Acne', '250.000', '13:00:00', 0),
+(5, 'Hair Treatment', '100.000', '08:00:00', 0),
+(6, 'Hair Treatment', '100.000', '10:00:00', 0),
+(7, 'Hair Treatment', '100.000', '12:00:00', 0),
+(8, 'Hair Treatment', '100.000', '13:00:00', 0),
+(9, 'Facial', '150.000', '08:00:00', 0),
+(10, 'Facial', '150.000', '10:00:00', 0),
+(11, 'Facial', '150.000', '13:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -102,17 +98,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `nama`, `tgl_lahir`, `role`) VALUES
-(1, 'admin', 'admin', 'Administrator', '2025-11-13', 'admin');
+(1, 'luluk', 'luluk', 'Luluk', '2025-10-30', 'pelanggan'),
+(2, 'admin', 'admin', 'Admin', '2025-11-13', 'admin'),
+(3, 'mayden', 'mayden', 'Maya', '2025-11-12', 'pelanggan'),
+(4, 'syifa', 'syifa', 'Syifa', '2025-11-13', 'pelanggan');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `master_treatment`
---
-ALTER TABLE `master_treatment`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `transaction`
@@ -126,8 +119,7 @@ ALTER TABLE `transaction`
 -- Indexes for table `treatment`
 --
 ALTER TABLE `treatment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_treatment_master` (`id_layanan`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -140,28 +132,22 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `master_treatment`
---
-ALTER TABLE `master_treatment`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `treatment`
 --
 ALTER TABLE `treatment`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -173,12 +159,6 @@ ALTER TABLE `user`
 ALTER TABLE `transaction`
   ADD CONSTRAINT `fk_treatment` FOREIGN KEY (`id_treatment`) REFERENCES `treatment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `treatment`
---
-ALTER TABLE `treatment`
-  ADD CONSTRAINT `fk_treatment_master` FOREIGN KEY (`id_layanan`) REFERENCES `master_treatment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
